85caccb93f9664c5edba39d8176de0e6cfaa3fe1
João Pedro Rego Maciel <68111919+joaoprmaciel@users.noreply.github.com>
2021-07-19 21:25:06 +0000

    Update SCRIPT_5.sql

--- SCRIPT_5.sql
+++ SCRIPT_5.sql
@@ -1,3 +1,4 @@
-SELECT e.ID, e.NAME AS EMPLOYEE, d.NAME AS DEPARTMENT FROM employee AS e
-JOIN department AS d ON e.DEPARTMENT_ID = d.ID
-WHERE e.CHIEF_ID IS NULL;
+SELECT a.* FROM employee a
+LEFT JOIN employee b ON (
+    b.id = a.chief_id AND b.department_id = a.department_id
+) WHERE b.id IS NULL
